ead078df3c17ab4229aa221dafb3d1aa1ffc92c2
Mohammad Nihal <mohammad.nihal@gmail.com>
2023-12-29 15:25:20 +0100

    day 7 - added files, associated functions

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-todos = []
-
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt','r')
+            todos = file.readlines()
+            file.close()   # always close open files, after processing
+
             todos.append(todo)
+
+            file = open('todos.txt','w')
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open('todos.txt','r')
+            todos = file.readlines()
+            file.close()   # always close open files, after processing
+
             for index, item in enumerate(todos):
                 row=f"{index + 1 }-{item}"  # f string
                 print(row)
