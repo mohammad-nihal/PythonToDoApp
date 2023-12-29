347647c1ae66337d39b079d60f56fec20a61856f
Mohammad Nihal <mohammad.nihal@gmail.com>
2023-12-29 13:48:22 +0100

    used f-string

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show,edit or exit:")
+    user_action = input("Type add, show, edit or exit:")
     user_action = user_action.strip()
     match user_action:
         case 'add':
@@ -13,7 +13,6 @@ while True:
                 print(row)
         case 'edit':
             number = int(input("Number of the todo to edit: "))
-            number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
         case 'exit':
