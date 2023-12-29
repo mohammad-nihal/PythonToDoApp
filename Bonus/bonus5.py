abbc7e38ec8687227bd9a024427242ba92addf35
Mohammad Nihal <mohammad.nihal@gmail.com>
2023-12-29 15:01:18 +0100

    bonus 5 udpated to include for loop with three variables example

--- Bonus/bonus5.py
+++ Bonus/bonus5.py
@@ -5,3 +5,9 @@ waiting_list.sort() #sort list
 for index, item in enumerate(waiting_list):
     row = f"{index + 1}.{item.capitalize()}"
     print(row)
+
+
+#you can also use three variables
+buttons = [('John', 'Sen', 'Morro'), ('Lin', 'Ajay', 'Filip'), ('Chen', 'Vijay', 'Malte')]
+for first, second, third in buttons:
+    print(first, second, third)
